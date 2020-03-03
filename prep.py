363d81aa578e0840d15231f480829e640e98c108
Nathan Cassee <n.w.cassee@tue.nl>
2020-03-03 11:20:49 +0100

    Fixed a small bug in prep.py

--- prep.py
+++ prep.py
@@ -11,17 +11,9 @@ releases = []
 if __name__ == "__main__":
     with open('jquery_releases.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        line_count = 0
         for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                releases.append(row)
-            line_count += 1
+            releases.append(row)
 
-
-        print(f'Processed {line_count} lines.')
 
     for release in releases:
         command = f"git clone -b '{release['tag']}' --single-branch --depth 1 https://github.com/jquery/jquery.git {release['tag']}"
