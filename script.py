47bdb370d61375ce24b61e972a514fdb94af7497
Leon Vitanos <leon.vitanos@gmail.com>
2021-03-12 00:49:06 +0100

    finding coverage using matches with 2 instances only

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import os
 import itertools
 import subprocess
 import json
+import pathlib
 
 if __name__ == "__main__":
     #Get the pairs of directories of the current path
@@ -10,21 +11,33 @@ if __name__ == "__main__":
     paths = [x for x in paths if os.path.isdir(os.path.join(rootdir, x))]
     pairs = list(itertools.combinations(paths, 2))
     
+    n_code = []
+
     for path in paths:
+        #Run cloc, with .json as output, and JavaScript as the selected language
         result = subprocess.run(['cloc', '-json', '-include-lang=JavaScript', os.path.join(rootdir, path)], stdout=subprocess.PIPE)
         lines_of_code = json.loads(result.stdout)["JavaScript"]["code"]
+        n_code.append(lines_of_code)
         #print(lines_of_code)
 
     for pair in pairs:
-        result = subprocess.run(["jsinspect", "-reporter", "json", "-t", "30",  pair[0], pair[1]], stdout=subprocess.PIPE, cwd=rootdir)
+        #Run jsinspect with specific threshold. Request output to be at json form. Ignore files which cannot be read by jsinspect
+        result = subprocess.run(["jsinspect", "-reporter", "json", "-t", "30", "--ignore", 'src/intro.js|src/outro.js',
+          pair[0], pair[1]], stdout=subprocess.PIPE, cwd=rootdir)
         out = json.loads(result.stdout)
+        sim_n_code = 0
         for match in out:
-            print(match["instances"])
-            print(len(match["instances"]))
-            #print(match["instances"])
-            #print(i)
-            #break
-        #out = json.loads(result.stdout)
-        #print(out)
-        break
-    
+            if len(match["instances"])==2: # for now only dealing with the simple case where there is a match with 2 instances
+                #Check if the version of the paths is the same or not
+                if pathlib.Path(match["instances"][0]["path"]).parts[0] == pathlib.Path(match["instances"][1]["path"]).parts[0]:
+                    print('same')
+                else:
+                    # i think n_of lines will not be always the same
+                    # i think we need the diff of both instances
+                    sim_n_code=(match["instances"][0]["lines"][1]-match["instances"][0]["lines"][0] +
+                    match["instances"][1]["lines"][1]-match["instances"][1]["lines"][0] + 2)
+        
+        index1 = paths.index(pair[0])
+        index2 = paths.index(pair[1])
+        print("Path 1 #code:", n_code[index1], "Path 2 #code:", n_code[index2], "Pair #sim:", sim_n_code, 
+        "Coverage:", sim_n_code/(n_code[index1]+n_code[index2]))
